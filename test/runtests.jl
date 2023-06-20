f8e9158e1274e18965592bacc9acefc1e6b2b382
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2023-06-20 17:49:39 +0200

    Setup tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,26 @@
+using Aqua
+using Documenter
 using GraphsBase
+using JET
+using JuliaFormatter
 using Test
 
-@testset "GraphsBase.jl" begin
-    # Write your tests here.
+DocMeta.setdocmeta!(GraphsBase, :DocTestSetup, :(using GraphsBase); recursive=true)
+
+@testset verbose = true "GraphsBase.jl" begin
+    @testset "Code quality (Aqua.jl)" begin
+        Aqua.test_all(GraphsBase)
+    end
+    @testset "Formatting (JuliaFormatter.jl)" begin
+        @test format(GraphsBase; verbose=false, overwrite=false)
+    end
+    @testset "Doctests (Documenter.jl)" begin
+        doctest(GraphsBase)
+    end
+    @testset "Static checking (JET.jl)" begin
+        if VERSION >= v"1.9"
+            JET.test_package(GraphsBase; target_modules=(GraphsBase,))
+        end
+    end
+    # Put the actual tests here
 end
