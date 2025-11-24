7fc1f6f015ac4246dba2ab308398b91f6acf55ee
Documenter.jl <documenter@juliadocs.github.io>
2025-11-24 02:08:59 +0000

    build based on a94f90f

---
+++ previews/PR39/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#GraphsBase","page":"Home","title":"GraphsBase","text":"Documentation for GraphsBase.","category":"section"},{"location":"#API-reference","page":"Home","title":"API reference","text":"","category":"section"},{"location":"#Index","page":"Home","title":"Index","text":"","category":"section"},{"location":"#GraphsBase.GraphsBase","page":"Home","title":"GraphsBase.GraphsBase","text":"GraphsBase\n\nThe basic interface and graph types for the JuliaGraphs ecosystem.\n\n\n\n\n\n","category":"module"}]
+}
